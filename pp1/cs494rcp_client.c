2b9b2b0e770a22eea6335793d49cb4c5896aaa7c
Ovidiu Mura <mura@augusta.cs.pdx.edu>
2019-02-16 21:51:57 -0800

    adding final draft of pp1

---
+++ pp1/cs494rcp_client.c
@@ -0,0 +1,161 @@
+/*
+ * Class: CS594
+ * Student: Ovidiu Mura
+ * Date: 02/15/2019
+ */
+
+#include "packets.h"
+
+long PORT = 1080;
+char *fname;
+int sockfd;
+long file_size=0;
+
+
+struct sockaddr_in servaddr, cliaddr;
+struct Packet_ACK_C ack_c;
+struct Packet_SYN_C syn;
+char *bname;
+char pkt[1024];
+
+
+int handshake(struct sockaddr_in servaddr)
+{
+  syn.type = S;
+  syn.pkt_len = sizeof(syn);
+  char *dup = strdup(fname);
+  bname = basename(dup);
+  strcpy(syn.filename, fname);
+  int n, len;
+  char *ser = serialize_s(&syn);
+  sendto(sockfd,ser,1024,MSG_PEEK|MSG_TRUNC,(const struct sockaddr*)&servaddr,sizeof(servaddr));
+  free(ser);
+  n = recvfrom(sockfd, NULL, 0, MSG_PEEK | MSG_TRUNC, (struct sockaddr*)(&servaddr),&len);
+  //Read packet
+  recvfrom(sockfd, pkt, n, 0, (struct sockaddr*)(&servaddr),&len);
+
+  struct Packet_SYN_ACK_C *p = desirealize_r(pkt);
+  file_size = p->file_size;
+  free(p);
+
+  ack_c.type = W;
+  char *ser_w = serialize_w(&ack_c);
+  sendto(sockfd,ser_w,sizeof(ack_c),MSG_PEEK|MSG_TRUNC,(const struct sockaddr*)&servaddr,sizeof(servaddr));
+  free(ser_w);
+  return 1;
+}
+
+struct Packet_ACK_D pad;
+char *pkt_ack_d;
+char pktt[2048];
+int receive_data()
+{
+  int len, n;
+  char *buffer=NULL;
+  int size=0;
+  char *temp;
+  char *a;
+  int b=0;
+  char *tmp;
+  char *ll;
+  int max = file_size/1024 + ((file_size%1024==0)? 0:1);
+  for (int i=0; i<max; i++)
+  {
+    n = recvfrom(sockfd, NULL, 0, MSG_PEEK | MSG_TRUNC, (struct sockaddr*)&servaddr, &len);
+    tmp = buffer;
+    recvfrom(sockfd, pktt, n, 0, (struct sockaddr*)(&servaddr),&len);
+
+    struct Packet_DATA_D *dd = desirealize_d(pktt);
+    if(dd->seq_num != i)
+      i--;
+    if(dd->seq_num == i)
+    {
+      size += dd->pkt_len;
+      buffer = malloc(size*sizeof(char));
+
+      if(tmp!=NULL)
+        memcpy(buffer, tmp, size);
+
+      b=size-dd->pkt_len;
+      cpy(&dd->data, buffer, b, dd->pkt_len);
+      temp = buffer;
+      free(dd);
+    }
+    pad.type = A;
+    pad.seq_num = i;
+    pkt_ack_d = serialize_ad(&pad);
+
+    // send acknowledge
+    int ll = sizeof(servaddr);
+    struct sockaddr* ad = (struct sockaddr*)&servaddr;
+    long ss = sizeof(pad);
+    sendto(sockfd,pkt_ack_d,ss,MSG_CONFIRM,ad, ll);
+    free(pkt_ack_d);
+  }
+
+  strcat(bname, ".out");
+  FILE *f=fopen(bname,"wb");
+  fwrite(temp, size, 1, f);
+  fclose(f);
+  return -1;
+}
+
+struct Packet_ACK_CC a_cc;
+
+void close_connection_client()
+{ 
+  int n, len;
+  n = recvfrom(sockfd, NULL, 0, MSG_PEEK|MSG_TRUNC, (struct sockaddr*)(&servaddr), &len);
+
+  recvfrom(sockfd, pkt, n, 0, (struct sockaddr*)(&servaddr),&len);
+  struct Packet_ACK_CC *acc = desirealize_cc(pkt);
+  a_cc.type = C;
+  char * packet_ack_cc = serialize_cc(&a_cc);
+  sendto(sockfd, packet_ack_cc, 100, MSG_PEEK|MSG_TRUNC, (const struct sockaddr*)&servaddr, sizeof(servaddr));
+  if(close(sockfd) != 0)
+    fprintf(stderr, "failed to close the client socket\n");
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 4)
+  {
+    printf("Usage: %s <server_ip> <port> <file>\n", argv[0]);
+  } else {
+    if(!isServerIpFormat(argv[1])) {
+	    printf("server_ip is not correct format\n");
+	    exit(-1);
+    }
+    if(!isNumber(argv[2])){
+	printf("port is not a number\n");
+	exit(-1);
+    } else {
+      PORT = atol(argv[2]);
+    }
+    if(!validateFilePath(argv[3])) {
+      fprintf(stderr, "the file provided is not valid\n");
+      usage(argv[0]);
+      exit(1);
+    } else {
+      fname = argv[3];
+    }
+    
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+      perror("socket creation failed");
+      exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr.s_addr);
+
+    handshake(servaddr);
+    receive_data();
+    close_connection_client();
+  }
+  return 0;
+}
---
+++ pp1/utils.c
@@ -0,0 +1,263 @@
+/*
+ * Class: CS594
+ * Student: Ovidiu Mura
+ * Date: 02/15/2019
+ */
+
+#include "packets.h"
+
+timer_t timer;
+struct sigevent sigevt;
+struct itimerspec timerspec;
+int thread_param = 31415;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int expired=0;
+
+struct thread_params {
+  int sockid;
+  char *pkt;
+  struct sockaddr *addr;
+};
+
+void usage(char *a)
+{
+  printf("Usage: %s <server_ip> <port> <file>\n", a);
+}
+
+int isNumber(char* s)
+{
+  int len = strlen(s);
+  for (int i=0; i<len; i++)
+    if (!isdigit(s[i]))
+    {
+      printf("error: the entered <port> is not a number\n");
+      exit(1);
+    }
+}
+
+int validateFilePath(const char *f)
+{
+  FILE *file;
+  if(file = fopen(f, "r"))
+  {
+    fclose(file);
+    return 1;
+  } else return 0;
+}
+
+int isServerIpFormat(char *ip)
+{ char delim[] = ".";
+  char *ptr = strtok(ip, delim);
+  int i = 0;
+  while(ptr != NULL)
+  {
+    ptr = strtok(NULL, delim);
+    i++;
+  }
+  if(i==4)
+    return 1;
+  else
+    return 0;
+}
+
+void cpy(void *src, void *dest, int i, int j)
+{
+  char *s = (char*)src;
+  char *d = (char*)dest;
+  for(int k=i; k<(i+j); k++)
+    d[k] = s[k-i];
+}
+
+
+// ------------------- timer ------------------
+void timer_thread(union sigval sv)
+{
+  int len;
+  pthread_mutex_lock(&mutex);
+  expired = 1;
+  pthread_mutex_unlock(&mutex); 
+}
+
+void set_signal()
+{
+  sigevt.sigev_notify = SIGEV_THREAD;
+  sigevt.sigev_notify_function = timer_thread;
+  sigevt.sigev_value.sival_ptr = &thread_param;
+  sigevt.sigev_notify_attributes = NULL;
+}
+
+void setup_one_shot_timer(int i)
+{
+  timerspec.it_value.tv_sec=i;
+  timerspec.it_value.tv_nsec=0;
+  timerspec.it_interval.tv_sec=0;
+  timerspec.it_interval.tv_nsec=0;
+}
+
+
+void arm_timer()
+{
+  timer_create(CLOCK_MONOTONIC, &sigevt, &timer);
+  timer_settime(timer, 0, &timerspec, 0);
+}
+
+void disarm_timer()
+{
+  timerspec.it_value.tv_sec=0;
+  timer_settime(timer, 0, &timerspec, 0);
+//  setNotExpired();
+}
+
+int getExpired()
+{
+  return expired;
+}
+
+void setNotExpired()
+{
+//  pthread_mutex_lock(&mutex);
+  expired=0;
+//  pthread_mutex_unlock(&mutex);
+  printf("info: expired set to 0\n");
+}
+// --------------------- timer ----------------------
+
+int find_size(char file_name[])
+{
+  FILE* fp = fopen(file_name, "r");
+  if(fp == NULL)
+  {
+    printf("File Not Found!\n");
+    return -1;
+  }
+  fseek(fp, 0L, SEEK_END);
+  int result = ftell(fp);
+  fclose(fp);
+  return result;
+}
+
+// ---------------------- desirealize ----------------------
+struct Packet_SYN_C *desirealize_s(char *b)
+{
+  struct Packet_SYN_C *p = malloc(sizeof(struct Packet_SYN_C));
+  memcpy(&((struct Packet_SYN_C*)p)->type, &((struct Packet_SYN_C*)b)->type, sizeof(char));
+  memcpy(&((struct Packet_SYN_C*)p)->pkt_len, &((struct Packet_SYN_C*)b)->pkt_len, sizeof(short));
+  memcpy(&((struct Packet_SYN_C *)p)->filename,&((struct Packet_SYN_C *)b)->filename, sizeof(char)*100);
+  return p;
+}
+
+struct Packet_ACK_C *desirealize_w(char *b)
+{
+  struct Packet_ACK_C *p = malloc(sizeof(char));
+  memcpy(&((struct Packet_ACK_C*)p)->type, b, sizeof(char));
+  printf("type: packet_ack_c: %d\n",p->type);
+  return p;
+}
+
+
+struct Packet_SYN_ACK_C *desirealize_r(char *b)
+{
+  struct Packet_SYN_ACK_C *p = malloc(sizeof(struct Packet_SYN_ACK_C));
+  memcpy(&((struct Packet_SYN_ACK_C*)p)->type, &((struct Packet_SYN_ACK_C*)b)->type, sizeof(char));
+  memcpy(&((struct Packet_SYN_ACK_C*)p)->file_size, b+1, sizeof(long));
+  return p;
+}
+
+char * serialize_r(struct Packet_SYN_ACK_C *ppp)
+{
+  long sii = sizeof(char)+sizeof(long)+4;
+  char *r = malloc(sii);
+  int off = 0;
+  memcpy(r, &ppp->type, sizeof(char));
+  off=sizeof(char);
+  memcpy(r+off, &ppp->file_size, sizeof(long)+2);
+  return r;
+}
+
+
+char * serialize_s(struct Packet_SYN_C *p)
+{
+  char *s = malloc(sizeof(char)+sizeof(short)+sizeof(char)*100);
+  int off=0;
+  memcpy(s, &p->type, sizeof(char));
+  off = sizeof(char);
+  memcpy(s+off+1, &p->pkt_len, sizeof(short));
+  off += sizeof(short);
+  memcpy(s+off+1, &p->filename, sizeof(char)*98);
+  return s;
+}
+
+char * serialize_w(struct Packet_ACK_C *p)
+{
+  char *m = malloc(sizeof(char));
+  memcpy(m, &p->type, sizeof(char));
+  return m;
+}
+
+char *serialize_d(struct Packet_DATA_D *p)
+{
+  char *d = malloc(sizeof(char)+sizeof(int)+sizeof(short)+sizeof(char)*1024);
+  int off=0;
+  memcpy(d, &p->type, sizeof(char));
+  off = sizeof(char);
+  memcpy(d+off, &p->seq_num, sizeof(int));
+  off += sizeof(int);
+  memcpy(d+off, &p->pkt_len, sizeof(short));
+  off += sizeof(short);
+  memcpy(d+off, &p->data, sizeof(char)*1024);
+  return d;
+}
+
+struct Packet_DATA_D *desirealize_d(char *d)
+{
+  struct Packet_DATA_D *p = malloc(sizeof(struct Packet_DATA_D));
+  int off=0;
+  memcpy(&p->type, d, sizeof(char));
+  off+=sizeof(char);
+  memcpy(&p->seq_num, d+off, sizeof(int));
+  off+=sizeof(int);
+  memcpy(&p->pkt_len, d+off, sizeof(short));
+  off+=sizeof(short);
+  memcpy(&p->data, d+off, 1024);
+  return p;
+}
+
+char *serialize_ad(struct Packet_ACK_D *ad)
+{
+  char *p = malloc(sizeof(char)+sizeof(int));
+  int off=0;
+  memcpy(p, &ad->type, sizeof(char));
+  off+=sizeof(char);
+  memcpy(p+off, &ad->seq_num, sizeof(int));
+  return p;
+}
+
+struct Packet_ACK_D *desirealize_ad(char *ad)
+{
+  struct Packet_ACK_D *p = malloc(sizeof(struct Packet_DATA_D));
+  int off=0;
+  memcpy(&p->type, ad, sizeof(char));
+  off+=sizeof(char);
+  memcpy(&p->seq_num, ad+off, sizeof(int));
+  return p;
+}
+
+char *serialize_cc(struct Packet_ACK_CC *cc)
+{
+  char *p = malloc(sizeof(char)+sizeof(int));
+  int off=0;
+  memcpy(p, &cc->type, sizeof(char));
+  off+=sizeof(char);
+  memcpy(p+off, &cc->seq_num, sizeof(int));
+  return p;
+}
+
+struct Packet_ACK_CC *desirealize_cc(char *cc)
+{
+  struct Packet_ACK_CC *p = malloc(20);
+  int off=0;
+  memcpy(&p->type, cc, sizeof(char));
+  off+=sizeof(char);
+  memcpy(&p->seq_num, cc+off, sizeof(int));
+  return p;
+}
